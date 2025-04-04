8ee8e5d3656879faca4728cf29d4147b586d864e
Di <dihuynh2302@gmail.com>
2025-04-03 21:08:57 -0400

    (wip) config

--- zkml/__init__.py
+++ zkml/__init__.py
@@ -1,25 +1,27 @@
 import numpy as np
 
 from . import protocol
-from .client_manager import RoundRobinClientManager
-from .verifier import NopVerifier
+from .config import Config
 
-client_manager = None
-verifier = None
+config = Config()
 
-async def init(addresses):
-    global client_manager, verifier
-    client_manager = RoundRobinClientManager(addresses)
-    verifier = NopVerifier()
+async def init(addresses, backend=None, verifier=None, client_manager=None):
+    config.configure(addresses, backend, verifier, client_manager)
+    client_manager = config.client_manager_instance()
     await client_manager.ready()
 
 async def matmul(a, b):
     assert isinstance(a, np.ndarray), "Numpy only"
     assert isinstance(b, np.ndarray), "Numpy only"
 
+    backend = config.backend_instance()
+    return backend.matmul()
+
+    # this is remote backend code
     reader, writer = client_manager.client()
     await protocol.send_matrix(a, writer)
     await protocol.send_matrix(b, writer)
     evidence, c = await protocol.receive_matrix(reader)
     assert verifier.verify(evidence)
+
     return c
---
+++ zkml/config.py
@@ -0,0 +1,59 @@
+class Config:
+    def __init__(self):
+        self.backend = "trust"  # {"local", "trust", "freivalds"}
+        self.client_manager = "roundrobin"
+        self.verifier = "noop"
+        self.addresses = []
+        self._backend_instance = None
+        self._verifier_instance = None
+        self._client_manager_instance = None
+    
+    def configure(self, addresses=None, backend=None, verifier=None, client_manager=None, **backend_options):
+        if addresses is not None:
+            self.addresses = addresses
+        if backend is not None:
+            self.backend = backend
+            self._backend_instance = None
+        if verifier is not None:
+            self.verifier = verifier
+            self._verifier_instance = None
+        if client_manager is not None:
+            self.client_manager = client_manager
+            self._client_manager_instance = None
+        if backend_options:
+            self.backend_options.update(backend_options)
+    
+    def backend_instance(self):
+        if self._backend_instance is None:
+            self._backend_instance = self._initialize_backend()
+        return self._backend_instance
+    
+    def verifier_instance(self):
+        if self._verifier_instance is None:
+            self._verifier_instance = self._initialize_verifier()
+        return self._verifier_instance
+    
+    def client_manager_instance(self):
+        if self._client_manager_instance is None:
+            self._client_manager_instance = self._initialize_client_manager()
+        return self._client_manager_instance
+    
+    def _initialize_backend(self):
+        if self.backend == "local":
+            pass
+        else:
+            raise ValueError("Unknown backend. Backend options: local, trust, freivalds")
+    
+    def _initialize_verifier(self):
+        if self.verifier == "noop":
+            from .verifier import NopVerifier
+            return NopVerifier()
+        else:
+            raise ValueError("Unknown verifier")
+    
+    def _initialize_client_manager(self):
+        if self.client_manager == "roundrobin":
+            from .client_manager import RoundRobinClientManager
+            return RoundRobinClientManager(self.addresses)
+        else:
+            raise ValueError("Unknown client manager")
